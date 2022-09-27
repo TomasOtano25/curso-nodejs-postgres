e6c2b5ef1a7ea57fef90ec89a119c576f3cf1f3e
tomasotano25 <tomasotano25@gmail.com>
2022-09-27 09:39:49 -0400

    Clase 02 configurar el entorno de trabajo

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,19 +1,80 @@
 const express = require('express');
 
-const router = express.Router();
+const UserService = require('../services/user.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createUserSchema,
+  updateUserSchema,
+  getUserSchema,
+} = require('../schemas/user.schema');
 
-// Ruta: http://localhost:3000/users?limit=50&offset=200
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (!!limit && !!offset) {
-    // --
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
+const router = express.Router();
+const service = new UserService();
+
+router.get('/', async (req, res) => {
+  const users = await service.find();
+  res.json(users);
 });
+
+router.get(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+
+    const newUser = await service.create(body);
+
+    res.status(201).json({ message: 'created', data: newUser });
+  }
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+
+      const user = await service.update(id, body);
+
+      res.json({
+        message: 'update',
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res) => {
+    const { id } = req.params;
+
+    const userId = await service.delete(id);
+    res.json({
+      message: 'update partial',
+      userId,
+    });
+  }
+);
 
 module.exports = router;
