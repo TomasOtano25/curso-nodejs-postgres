0d665b859078111a0152f3e3199bddf253126ebe
tomasotano25 <tomasotano25@gmail.com>
2022-09-27 12:33:33 -0400

    Clase 08 variables de ambiente

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,20 @@
 const { Pool } = require('pg');
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const pool = new Pool({
-  host: 'localhost',
-  post: 5432,
-  user: 'tomas',
-  password: 'admin123',
-  database: 'my_store',
+  connectionString: URI,
 });
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   post: 5432,
+//   user: 'tomas',
+//   password: 'admin123',
+//   database: 'my_store',
+// });
 
 module.exports = pool;
