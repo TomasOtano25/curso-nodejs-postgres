4ed12ece38d9de7eed825c2b69171e97da8c3ead
tomasotano25 <tomasotano25@gmail.com>
2022-09-27 11:53:57 -0400

    Clase 06 integracion de node-postgres

---
+++ libs/postgres.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+async function getConnection() {
+  const client = new Client({
+    host: 'localhost',
+    post: 5432,
+    user: 'tomas',
+    password: 'admin123',
+    database: 'my_store',
+  });
+
+  await client.connect();
+
+  return client;
+}
+
+module.exports = getConnection;
